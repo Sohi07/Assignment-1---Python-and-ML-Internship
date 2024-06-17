cd87b0ff6478b3c9af045acf39bf8f4aca00c24e
Sohini_Das <sohinid474@gmail.com>
2024-06-18 01:25:36 +0530

    adding question9

---
+++ Question9.py
@@ -0,0 +1,6 @@
+str1=input("Enter the string: ")
+str2=input("Enter the substring: ")
+if str2 in str1:
+    print("Substirng is present in the string")
+else:
+    print("Substring is not present in the string")
