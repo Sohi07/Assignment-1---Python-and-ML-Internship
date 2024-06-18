ff8b9923e86b4957a2530142f65db882b24bcca8
Sohini_Das <sohinid474@gmail.com>
2024-06-18 21:39:59 +0530

    adding question24

---
+++ Question24.py
@@ -0,0 +1,23 @@
+def calculator(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+
+        if num2 == 0:
+            return "Error: Division by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operator"
+
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+result = calculator(num1, num2, operator)
+print(f"{num1} {operator} {num2} = {result}")
